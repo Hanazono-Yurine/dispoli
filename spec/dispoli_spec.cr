1dd25df851d65c01e9f5e8b18fb51a22fa3b2794
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-10-23 05:30:16 -0300

    chore: Update spec

--- spec/dispoli_spec.cr
+++ spec/dispoli_spec.cr
@@ -6,7 +6,7 @@ describe JsonParser do
 		it "JsonParser return test" do
 			testJson = %({"hr":"16:09","vs":[{"p":"82512","a":true,"ta":"2023-10-22T19:08:59Z","py":-23.5662345,"px":-46.738654499999996,"sv":null,"is":null},{"p":"82500","a":true,"ta":"2023-10-22T19:08:52Z","py":-23.571716,"px":-46.709317999999996,"sv":null,"is":null}]})
 
-			returnValue = JsonParser.push_to_map(testJson)
+			returnValue = JsonParser.get_raw_map(testJson)
 
 			returnValue["hr"].should eq("16:09")
 			returnValue["vs"][0]["p"].should eq("82512")
