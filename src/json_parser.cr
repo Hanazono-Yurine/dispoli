92da60360739916609c20b592cf88878a0514c47
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-12-02 23:46:03 -0300

    feat(json): Add a really basic parser for SPTrans api
    Its not a parser, its basic only remove unnecessary fields in the json

--- src/json_parser.cr
+++ src/json_parser.cr
@@ -3,8 +3,22 @@ require "json"
 module JsonParser
 	extend self
 
-	def get_raw_map(json : String)
+	def get_better_json(json : String)
 		data = Hash(String, JSON::Any).from_json(json)
+		better_json = [] of JSON::Any
+		i = 0
+		while i < 3
+			arriveJsonString = JSON.build do |json|
+				json.object do
+					json.field "bus", data["p"]["l"][i]["c"]
+					json.field "arrive", data["p"]["l"][i]["vs"][0]["t"]
+				end
+			end
+			better_json << JSON.parse(arriveJsonString)
+			i += 1
+		end
+		puts better_json.to_json
+		better_json.to_json
 	end
 
 end
