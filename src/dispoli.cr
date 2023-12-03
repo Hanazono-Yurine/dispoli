82daffde9ac802438726f71e27f8eb8c9fd7869d
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-12-02 23:52:46 -0300

    Finally add the main function
    Making all things work together

--- src/dispoli.cr
+++ src/dispoli.cr
@@ -4,7 +4,25 @@ require "./webserver/webserver.cr"
 module Dispoli
 	VERSION = "0.1.0"
 
-	test = Sptrans.new("")
-	if test.login?
+	handlers = Webserver::Handlers.new "xxxxx"
+
+	server = HTTP::Server.new do |context|
+		handlers.url_type context
 	end
+
+	spawn do
+		loop do
+			handlers.sptrans_api.update_arrive_list()
+			sleep 1.minute
+		end
+	end
+
+	spawn do
+		address = server.bind_tcp 8080
+		puts "Listening on http://#{address}"
+		server.listen
+	end
+
+	sleep
+
 end
