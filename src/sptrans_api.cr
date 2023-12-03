e00cc0c5b9971c1d75e4986278d53e16a192f24c
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-12-02 23:49:39 -0300

    feat: Add method to get all arrive time prediction

--- src/sptrans_api.cr
+++ src/sptrans_api.cr
@@ -7,7 +7,10 @@ class Sptrans
 	@link = "api.olhovivo.sptrans.com.br/v2.1"
 	@api_token = ""
 	@auth_cookie = ""
+	@bus_stop = "340015329"
+	@arrive_time_json = ""
 
+	getter arrive_time_json
 	getter auth_cookie
 
 	def initialize(@api_token : String)
@@ -28,10 +31,14 @@ class Sptrans
 		return false
 	end
 
+	private def get_headers()
 		headers = HTTP::Headers.new
 		headers.add("Cookie", "apiCredentials=#{@auth_cookie}")
 	end
 
+	def update_arrive_list()
 		return Nil if !login?
+		response = HTTP::Client.get("http://#{@link}/Previsao/Parada?codigoParada=#{@bus_stop}", get_headers())
+		@arrive_time_json = JsonParser.get_better_json response.body
 	end
 end
